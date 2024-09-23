074e649bbcc9dc94a06e9ad7cb0a24842f48aeea
rmondal@crimson.ua.edu <rmondal@crimson.ua.edu>
2024-09-23 10:26:26 -0500

    Project1 added

---
+++ Fall2024-Assignment1-rmondal/Fall2024-Assignment1-rmondal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2024_Assignment1_Project1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
