679231de69782d6bec35eff056184479faed7b0d
rmondal5 <159176591+rmondal5@users.noreply.github.com>
2024-09-23 00:10:07 -0500

    Delete Fall2024-Assignment1-Project1 directory

--- Fall2024-Assignment1-Project1/Fall2024-Assignment1-Project1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Fall2024_Assignment1_Project1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
